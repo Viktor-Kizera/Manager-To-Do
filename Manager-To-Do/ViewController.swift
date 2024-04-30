a099bf47384d35a5f9156cc71a60de32d2b6bf0b
Viktor-Kizera <victorkizera2003@gmail.com>
2024-04-30 18:16:32 +0300

    Initial Commit

---
+++ Manager-To-Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manager-To-Do
+//
+//  Created by Viktor Kizera on 30.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
