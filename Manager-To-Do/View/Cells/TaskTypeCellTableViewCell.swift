f6d76fc72f31a6be8c3ac72b4d98bfc3aca45bfb
Viktor-Kizera <victorkizera2003@gmail.com>
2024-05-08 22:16:57 +0300

    work with xib files

---
+++ Manager-To-Do/View/Cells/TaskTypeCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskTypeCellTableViewCell.swift
+//  Manager-To-Do
+//
+//  Created by Viktor Kizera on 08.05.2024.
+//
+
+import UIKit
+
+class TaskTypeCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
